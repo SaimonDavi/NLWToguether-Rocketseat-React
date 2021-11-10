9278ba1752a8540a1026a6d17e7e5890f9bd87b5
Saimon Davi <saimon.dsfg@gmail.com>
2021-11-10 20:50:29 -0300

    Criando as páginas e atualizando da v5 para v6 do react-router-dom

--- newApp/letmeask/src/App.tsx
+++ newApp/letmeask/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Button } from './components/Button';
-import illustration from './assets/images/illustration.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
 
 function App() {
   return (
-    <div>
-      <h1>Oi</h1>
-      <Button>Olá</Button>
-      <img src={illustration} alt="imagem de ilustração" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<Home/>} />
+        <Route path='/rooms/new' element={<NewRoom/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
