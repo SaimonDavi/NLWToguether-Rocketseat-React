1df46c165890d8ec9487a3e82ac8eb837e8af027
Saimon Davi <saimon.dsfg@gmail.com>
2021-11-16 19:27:26 -0300

    Fazendo um novo status para criação de sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,7 @@ import '../styles/auth.css'
 import { Button } from '../components/Button'
 import { useAuth } from '../hooks/useAuth';
 
-
-export function Home(){
+export function Home() {
   const navigate = useNavigate();
 
   const { user, signInWithGoogle } = useAuth()
